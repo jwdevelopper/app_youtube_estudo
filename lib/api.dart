31726410343f30a4110fc3bb4eb5773905da2b81
jwdevelopper <58191292+jwdevelopper@users.noreply.github.com>
2021-07-23 13:32:20 -0300

    Inicio do desenvolvimento da classe api

--- lib/api.dart
+++ lib/api.dart
@@ -1,3 +1,15 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 const YOUTUBE_CHAVEAPI = "AIzaSyAgrqs7j5KPxXlln1Evf4Xl0b--4lCCjTY";
 const ID_CANAL = "UCplT2lzN6MHlVHHLt6so39A";
 const URL_BASE = "https://www.googleapis.com/youtube/v3/";
+
+class Api {
+
+  pesquisar(String pesquisa) async {
+    Uri uri = Uri.parse(URL_BASE + "search" + "?part=snippet" + "&type=video" + "&maxResults=20" 
+    + "&order=date" + "&key=$YOUTUBE_CHAVEAPI");
+    http.Response response = await http.get(uri);
+  }
+
+}
