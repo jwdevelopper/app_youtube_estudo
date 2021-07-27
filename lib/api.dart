1c8303ec55f34f3f42b52b97e94df62165717676
jwdevelopper <58191292+jwdevelopper@users.noreply.github.com>
2021-07-27 16:59:59 -0300

    Inicio da tela de lista de videos

--- lib/api.dart
+++ lib/api.dart
@@ -1,3 +1,4 @@
+import 'package:app_youtube/model/video.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 const YOUTUBE_CHAVEAPI = "AIzaSyAgrqs7j5KPxXlln1Evf4Xl0b--4lCCjTY";
@@ -6,10 +7,24 @@ const URL_BASE = "https://www.googleapis.com/youtube/v3/";
 
 class Api {
 
-  pesquisar(String pesquisa) async {
+  Future<List<Video>> pesquisar(String pesquisa) async {
     Uri uri = Uri.parse(URL_BASE + "search" + "?part=snippet" + "&type=video" + "&maxResults=20" 
-    + "&order=date" + "&key=$YOUTUBE_CHAVEAPI");
+    + "&order=date" + "&key=$YOUTUBE_CHAVEAPI" + "&channelId=$ID_CANAL" + "&q=$pesquisa");
     http.Response response = await http.get(uri);
+
+    if(response.statusCode == 200){
+      response.body;
+      Map<String, dynamic> dadosJson = json.decode(response.body);
+      List<Video> videos = dadosJson["items"].map<Video>((map){return Video.fromJson(map);}).toList();
+      return videos;
+      /*for(var video in videos){
+        print("TITULO DO VIDEO => " + video.titulo);
+      }*/
+
+      print("resultado => " + dadosJson["items"][0].toString());
+    } else {
+
+    }
   }
 
 }
--- lib/telas/inicio.dart
+++ lib/telas/inicio.dart
@@ -1,3 +1,5 @@
+import 'package:app_youtube/api.dart';
+import 'package:app_youtube/model/video.dart';
 import 'package:flutter/material.dart';
 
 class Inicio extends StatefulWidget {
@@ -6,12 +8,48 @@ class Inicio extends StatefulWidget {
 }
 
 class _InicioState extends State<Inicio> {
+
+  Future<List<Video>> _listarVideos() {
+    Api api = Api();
+    return api.pesquisar("");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Inicio"),
-      ),
+    
+    return FutureBuilder<List<Video>>(
+      builder: (context, snapshot){
+        switch(snapshot.connectionState){
+          case ConnectionState.none :
+          case ConnectionState.waiting :
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+            break;
+          case ConnectionState.active :
+          case ConnectionState.done :
+            if(snapshot.hasData){
+              return ListView.separated(
+                itemBuilder: (context, index){
+                  return Column(
+                    children: [
+                      Text("Testes")
+                    ],
+                  );
+                }, 
+                separatorBuilder: (context, index) {
+                 return Divider(color: Colors.red, height: 2,);
+                }, 
+                itemCount: snapshot.data.length
+                );
+            } else {
+              return Center(child: Text("Nenhum dado a ser exibido!"),);
+            }
+            break;
+            
+        }
+      },
+      future: _listarVideos(),
     );
   }
 }
